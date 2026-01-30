26ddcfc3323e980fd5804580d828d022f64754d5
Wilfredo Abregu <fabregu01@gmail.com>
2026-01-29 23:34:03 -0500

    first commit

---
+++ src/app/components/pregunta/botonera/botonera.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botonera',
+  standalone: true,
+  imports: [],
+  templateUrl: './botonera.component.html',
+  styleUrl: './botonera.component.css'
+})
+export class BotoneraComponent {
+
+}
